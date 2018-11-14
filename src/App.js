af319e4b63d0120fad4b0e7ee7f78add37fb07b4
Niraj Kakodkar <niraj.kakodkar@gmail.com>
2018-11-15 00:02:59 +0200

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Admin, Resource } from 'react-admin';
+import { UserList } from './users';
+import jsonServerProvider from 'ra-data-json-server';
+//import logo from './logo.svg';
+//import './App.css';
+//<img src={logo} className="App-logo" alt="logo" />
+
+const dataProvider = jsonServerProvider('http://jsonplaceholder.typicode.com');
+const App = () => (
+      <Admin dataProvider={dataProvider} >
+          <Resource name="users" list={UserList} />
+      </Admin>
+);
+
+export default App;
